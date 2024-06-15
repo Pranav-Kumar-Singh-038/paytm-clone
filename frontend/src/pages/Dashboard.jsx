93966a315cb61badb84790716784bea19fc3a603
Pranav-Kumar-Singh-073 <kumarharsh073@gmail.com>
2024-06-15 14:22:23 +0530

    added appbar component

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,11 +1,13 @@
 import Appbar from "../components/AppBar";
+import Balance from "../components/Balance";
+import Users from "../components/Users";
 
-export default function Dashboard()
-{
-    return(
-     <div>
-        <Appbar></Appbar>
-
-     </div>
+export default function Dashboard() {
+    return (
+        <div>
+            <Appbar></Appbar>
+            <Balance></Balance>
+            <Users></Users>
+        </div>
     )
 }
